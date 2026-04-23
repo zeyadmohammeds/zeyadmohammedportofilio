0fb042d8fc6a94a8a22104b42bd4b4d8ccc64688
Zeyad630 <Zeyad.shosha@outlook.com>
2026-04-23 23:11:57 +0200

    last update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -61,8 +61,7 @@ export const Route = createRootRoute({
       { name: "twitter:card", content: "summary_large_image" },
     ],
     links: [
-      { rel: "icon", type: "image/png", href: "/favicon.png" },
-      { rel: "shortcut icon", href: "/favicon.ico" },
+      { rel: "icon", href: "/favicon.io" },
       { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
